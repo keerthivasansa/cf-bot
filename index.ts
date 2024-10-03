85356c9b09adf5e842b8e752b2d777a03050bf8b
Keerthi Vasan <sakeerthi23@gmail.com>
2024-10-03 20:46:27 +0530

    feat: added table viz + pred caching for probrat

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import { Bot } from "./src/discord/bot";
+import CliTable3 from 'cli-table3';
 
 const application = {
     entry: [Bot],
--- src/discord/commands/probrat.ts
+++ src/discord/commands/probrat.ts
@@ -1,6 +1,8 @@
 import { EmbedBuilder, SlashCommandBuilder } from "discord.js";
 import { Command } from "../type";
 import { ClistsApi } from "$src/clists/client";
+import { db } from "$db/index";
+import CliTable3 from "cli-table3";
 
 export const probRatCmd: Command = {
     command: new SlashCommandBuilder()
@@ -17,36 +19,54 @@ export const probRatCmd: Command = {
 
         const contestId = msg.options.getNumber('contest_id');
 
-        const resp = await api.getContestRatings(contestId);
-        const problems = resp.objects;
-        console.log(problems);
+        const cachedProb = await db.selectFrom('problems').selectAll().where('contestId', '=', contestId).orderBy('index asc').execute();
 
-        const sorted = problems.sort((a, b) => {
-            if (a.url < b.url)
-                return -1;
-            else
-                return 1;
+        if (!cachedProb[0].predicted_rating) {
+            const resp = await api.getContestRatings(contestId);
+            const problems = resp.objects;
+
+            db.transaction().execute(async (tdb) => {
+                for (const p of problems) {
+                    const rating = Math.round(p.rating / 100) * 100;
+                    const probRating = Math.max(800, rating);
+                    const index = p.url.match("\/problem\/([A-Z]\\d?)$")[1];
+
+                    // update for short time.
+                    for (let i = 0; i < cachedProb.length; i++) {
+                        if (cachedProb[i].index == index)
+                            cachedProb[i].predicted_rating = probRating;
+                    }
+
+                    // cache for later use.
+                    await tdb.updateTable("problems").set({
+                        predicted_rating: probRating
+                    }).where(
+                        eb => eb.and([
+                            eb('contestId', '=', contestId),
+                            eb('index', '=', index)
+                        ])
+                    ).execute();
+                }
+            });
+        }
+
+        let table = new CliTable3({
+            head: ['Problem', 'Actual', 'Predicted'],
+            style: {
+                head: [], //disable colors in header cells
+                border: [], //disable colors for the border
+            },
+            colAligns: ['center', 'center', 'center'],
+            colWidths: [10, 12, 12], //set the widths of each column (optional)
         });
 
-        const fields = sorted.map(prob => {
-            const rating = Math.round(prob.rating / 100) * 100;
-            const probRating = Math.max(800, rating);
-            const name = prob.url.match("\/problem\/([A-Z]\\d?)$")[1];
-            return {
-                name: name + " ".repeat(21 - name.length) + (probRating || "-"),
-                value: " "
-            }
-        });
+        for (const prob of cachedProb)
+            table.push([prob.index, prob.official_rating || '-', prob.predicted_rating || '-']);
 
-        console.log(fields);
-
-        const embed = new EmbedBuilder()
-            .setTitle(`${contestId} Problems Rating`)
-            .setColor('Blurple')
-            .addFields(fields)
+        const tableMsg = table.toString();
 
         await msg.reply({
-            embeds: [embed]
+            content: `\`\`\`Contest ${contestId} Ratings\n\n${tableMsg}\`\`\``
         });
     },
 }
