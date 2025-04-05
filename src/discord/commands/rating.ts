c490f5866b276f818333582057ddb67b04603b0b
Taran2005 <tarangadh@gmail.com>
2025-04-05 14:42:55 +0530

    feat: add handle option to rating command for non-registered users and improve user handle retrieval

--- src/discord/commands/rating.ts
+++ src/discord/commands/rating.ts
@@ -16,23 +16,36 @@ export const ratingCmd: Command = {
         .addUserOption(option => option
             .setName('user')
             .setDescription('Mention the user you want to check the rating of')
+        )
+        .addStringOption(option => option
+            .setName('handle')
+            .setDescription('Codeforces handle for non-registered users')
         ),
 
     async execute(msg, interaction) {
         const mention = msg.options.getUser('user');
+        const handle = msg.options.getString('handle');
         const selectedUser = mention ? mention : msg.user;
-
-        const user = await db.selectFrom('users').selectAll().where('discordId', '=', selectedUser.id).executeTakeFirst();
         const showEntire = msg.options.getBoolean('full');
-
-        if (!user)
-            return interaction.reply("User has not registered their codeforces handle!")
+        
+        let cfHandle: string;
+        
+        if (handle) {
+            // Use provided handle directly
+            cfHandle = handle;
+        } else {
+            // Look up handle in database for the mentioned user or message author
+            const user = await db.selectFrom('users').selectAll().where('discordId', '=', selectedUser.id).executeTakeFirst();
+            if (!user)
+                return interaction.reply("User has not registered their codeforces handle!");
+            cfHandle = user.handle;
+        }
 
         const cfApi = CFApiFactory.get();
-        const allRatings = await cfApi.getUserRatings(user.handle);
+        const allRatings = await cfApi.getUserRatings(cfHandle);
 
         if (allRatings.length === 0)
-            return interaction.reply("You have not participated in any contests yet!");
+            return interaction.reply("User has not participated in any contests yet!");
 
         const last10 = allRatings.slice(Math.max(0, allRatings.length - 10));
 
@@ -47,7 +60,7 @@ export const ratingCmd: Command = {
             selectedData.set(d, s.newRating);
         }
 
-        const chartUrl = new CFLineChart(selectedData)
+        const chart = new CFLineChart(selectedData)
             .labelMaxPoint()
             .setRangeBackground('RATING')
             .addOffsetToChart()
@@ -55,11 +68,11 @@ export const ratingCmd: Command = {
             .build()
             .toPNG();
 
-        const attachment = new AttachmentBuilder(chartUrl)
+        const attachment = new AttachmentBuilder(chart)
             .setName('canvas.png');
 
         const embed = new EmbedBuilder()
-            .setTitle(`${user.handle} - Rating`)
+            .setTitle(`${cfHandle} - Rating`)
             .setColor(getRatingColor(currRating))
             .setImage('attachment://canvas.png');
 
@@ -68,5 +81,4 @@ export const ratingCmd: Command = {
             files: [attachment]
         });
     },
-
 };
